e56ea4bc792cedbe2e8cfca216db126cfb1a0135
Lokesh <yokesh.ganesan@gmail.com>
2013-06-04 16:28:00 +0530

    Signed-off-by: Lokesh <yokesh.ganesan@gmail.com>

---
+++ site2/updateData.php
@@ -0,0 +1,37 @@
+
+	<img width="1920" height="1080" src="http://www.tristangaland.com/wp-content/uploads/sunset1.jpg" class="attachment-feature-image-2" alt="sunset" style="width: 1535px; height: 864px; left: 0px; top: -262.5px;">        <div id="contentWrap" class="mCustomScrollbar _mCS_5"><div class="mCustomScrollBox" id="mCSB_5" style="position:relative; height:100%; overflow:hidden; max-width:100%;"><div class="mCSB_container" style="position:relative; top:0;">
+	<div class="article">
+	<div id="vidThumbWrapper">
+	<img class="vidthumb" src="http://www.tristangaland.com/wp-content/uploads/gamins_link-755x425.jpg" style="opacity: 1;">
+	<img class="playVid" src="http://www.tristangaland.com/wp-content/themes/themeajaxclean/images/play.png" width="40" height="40" alt="playvid">
+	<p class="get_vimeo_link" style="display:none;">http://player.vimeo.com/video/59351320</p>
+	</div>
+	<div id="subWrapper">
+	<div id="textWrapper">
+	<h2>Better Cotton In Mali</h2>
+	<div class="content_article"><p><strong>DIRECTOR<br>
+	</strong>Alana Osbourne</p>
+	<p><strong>PRODUCTION</strong><br>
+	Better Cotton Initiative</p>
+	<p><strong>YEAR</strong><br>
+	2012</p>
+	<p><strong>INTRODUCTION<br>
+	</strong>Clip from a short film explaining the Better Cotton Initiative’s role and implication in promoting sustainable agriculture in Mali. This film was made with the help of the cotton producers of the Sikasso province and focuses on the transmission of knowledge.</p>
+	<p><strong>CREW<br>
+	</strong>Sound: Alana Osbourne<br>
+	Editing: Laura froidefond<br>
+	Music: Clément Marion<br>
+	Motion design: Pascal Giraud<br>
+	Mix: Thyl Mariage</p>
+	<p><strong>SHOT IN<br>
+	</strong>HD – 7D</p>
+	<p><b>&nbsp;</b></p>
+	<p><b>&nbsp;</b></p>
+	</div>
+	</div>
+	<div id="picsWrapper">
+	<a rel="shadowbox[Better Cotton In Mali]" class="photoboxlink" href="http://www.tristangaland.com/wp-content/uploads/lassina_link1.jpg"><img width="400" height="225" src="http://www.tristangaland.com/wp-content/uploads/lassina_link1-400x225.jpg" class="attachment-content_thumbs" alt="lassina_link"></a><a rel="shadowbox[Better Cotton In Mali]" class="photoboxlink" href="http://www.tristangaland.com/wp-content/uploads/plaine_backgound.jpg"><img width="400" height="225" src="http://www.tristangaland.com/wp-content/uploads/plaine_backgound-400x225.jpg" class="attachment-content_thumbs" alt="plaine_backgound"></a><a rel="shadowbox[Better Cotton In Mali]" class="photoboxlink" href="http://www.tristangaland.com/wp-content/uploads/ecole1.jpg"><img width="400" height="225" src="http://www.tristangaland.com/wp-content/uploads/ecole1-400x225.jpg" class="attachment-content_thumbs" alt="ecole"></a><a rel="shadowbox[Better Cotton In Mali]" class="photoboxlink" href="http://www.tristangaland.com/wp-content/uploads/bike1.jpg"><img width="400" height="225" src="http://www.tristangaland.com/wp-content/uploads/bike1-400x225.jpg" class="attachment-content_thumbs" alt="bike"></a><a rel="shadowbox[Better Cotton In Mali]" class="photoboxlink" href="http://www.tristangaland.com/wp-content/uploads/charette.jpg"><img width="400" height="225" src="http://www.tristangaland.com/wp-content/uploads/charette-400x225.jpg" class="attachment-content_thumbs" alt="charette"></a><a rel="shadowbox[Better Cotton In Mali]" class="photoboxlink" href="http://www.tristangaland.com/wp-content/uploads/berret.jpg"><img width="400" height="225" src="http://www.tristangaland.com/wp-content/uploads/berret-400x225.jpg" class="attachment-content_thumbs" alt="berret"></a><a rel="shadowbox[Better Cotton In Mali]" class="photoboxlink" href="http://www.tristangaland.com/wp-content/uploads/sunset1.jpg"><img width="400" height="225" src="http://www.tristangaland.com/wp-content/uploads/sunset1-400x225.jpg" class="attachment-content_thumbs" alt="sunset"></a>                    </div>
+	</div>
+	</div>
+	</div><div class="mCSB_scrollTools" style="position: absolute; display: block;"><div class="mCSB_draggerContainer" style="position:relative;"><div class="mCSB_dragger ui-draggable" style="position: absolute; height: 50px;"><div class="mCSB_dragger_bar" style="position: relative; line-height: 50px;"></div></div><div class="mCSB_draggerRail"></div></div></div></div></div>
+	
